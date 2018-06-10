67a0da13af5d4a82abc81a8f25f8b6774c0b8d77
Qiongsi Wu <wuqiongs@mel-16.syslab.sandbox>
2018-06-10 14:50:15 -0400

    Finishing script for different thread configs

--- run/repeat-diff-threads.sh
+++ run/repeat-diff-threads.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-THREADS=$1
-REPEATS=$2
-RUN_DIR=$3
-RESULT_DIR=$4
+REPEATS=$1
+RUN_DIR=$2
+RESULT_DIR=$3
 
-if test "$#" -ne 4
+NUM_THREAD_ARRY=(1 2 4 8 16 28 48 56)
+
+if test "$#" -ne 3
 then
-	echo "Usage: bash $0 <num_threads> <num_repeats> <run_dir> <results_dir>"
+	echo "Usage: bash $0 <num_repeats> <run_dir> <results_dir>"
 	exit 1
 elif test -z $1
 then 
-	echo "Usage: bash $0 <num_threads> <num_repeats> <run_dir> <results_dir>"
-	echo "<num_threads> must be positive"
+	echo "Usage: bash $0 <num_repeats> <run_dir> <results_dir>"
+	echo "<num_repeats> must be positive"
 	exit 1
 else
-	echo "Run all for $REPEATS times on $THREADS threads. Continue?[y/n]"
+	echo "Run all for $REPEATS times on ${#NUM_THREAD_ARRY[@]} configs of threads. Continue?[y/n]"
 	read input
 	if test $input != y
 	then 
@@ -23,3 +24,20 @@ else
 		exit 1
 	fi
 fi
+
+if test -d $RESULT_DIR
+then
+	echo "results dir exists. "
+else
+	echo "Creating results dir. "
+	mkdir $RESULT_DIR
+fi
+
+for i in "${NUM_THREAD_ARRY[@]}"
+do 
+	for j in `seq $REPEATS`
+	do
+		echo "Iteration $j out of $REPEATS on $i threads. "
+		$RUN_DIR/run-all.sh -c $i > $RESULT_DIR/${j}_${i}
+	done
+done
